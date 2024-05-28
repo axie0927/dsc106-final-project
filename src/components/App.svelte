4fc2409b92bb4bdf1216bd5eea1b50b17be7f9ef
Ammie Xie <atlus0927@Ammies-Air.lan>
2024-05-27 23:06:20 -0700

    added description

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -3,7 +3,8 @@
 </script>
 
 <main>
-  <h1>Crime Rates per Capita in the USA</h1>
+  <h1>Crimes in the USA</h1>
+  <p>For our visualizations, we have a map of the US showing the total number of the selected crimes that occurred during the selected year, with the circle sizes being proportional to the numbers. If you hover over each circle, it will show the exact number of crimes as well as the population of that specific city. Additionally, if you click on the bubbles, you will see a layered line graph that shows the number of crimes and the employment rate over the years 1975 to 2014.</p>
   <Map />
 
 </main>
