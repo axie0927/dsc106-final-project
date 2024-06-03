8d566bb1f6ad73b3406198ce41a069f07f2aa45d
cosmanatlol <manatomix@gmail.com>
2024-06-02 20:32:31 -0700

    heatmap

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -3,11 +3,13 @@
   import * as d3 from 'd3';
 
   let years = Array.from({ length: 2014 - 1975 + 1 }, (_, i) => (1975 + i).toString());
-  let selectedCrime = "violent_crimes";
-  let violent_crimes = ["violent_crimes", "homicides", "rapes", "assaults", "robberies"];
+  let selectedCrime = "Violent Crimes";
+  let violent_crimes = ["Violent Crimes", "Homicides", "Rapes", "Assaults", "Robberies"];
+  let violent_crimes_dict = {"Violent Crimes" :"violent_crimes", "Homicides": "homicides","Rapes": "rapes", "Assaults": "assaults","Robberies": "robberies"};
+  let selectedCrimeIndex = violent_crimes_dict[selectedCrime];
   let crimeData;
   let geojson;
-  let selectedYear = "1990";
+  let selectedYear = "1975";
   let path;
   const width = 1200;
   const height = 600;
@@ -17,6 +19,8 @@
   let selected = null;
   let previouslySelectedBubble = null;  
   let crime;
+  let corrData;
+  let curCorr;
 
   onMount(async () => {
     const response = await fetch('us-states.json');
@@ -28,7 +32,13 @@
     projection = d3.geoAlbersUsa().translate([width / 2, height / 2]);
     path = d3.geoPath().projection(projection);
 
+    const corrResponse = await fetch('city_correlation.json');
+    const corrJson = await corrResponse.json();
+    corrData = corrJson; 
+    curCorr = corrJson[selectedCrimeIndex]
+
     updateMap();
+    createHeatmap();
   });
 
   function updateMap() {
@@ -54,7 +64,7 @@
       .append('path')
       .attr('d', path)
       .attr('stroke', 'white')
-      .attr('fill', '#ddd');
+      .attr('fill', '#ddd'); // Set to none since we are not using heatmap colors here
 
     const bubble = svg.append("g").attr("class", "bubble");
 
@@ -83,7 +93,7 @@
 
     const mousemove = function (event, d) {
       const f = d3.format(",");
-      tooltip.html("<div style='color: #0072BC'><b>" + d.city + "</b></div><div>Number of " + selectedCrime.replace('_', ' ') + ": " + `${f(d[selectedCrime])}` + "</div>"
+      tooltip.html("<div style='color: #0072BC'><b>" + d.city + `</b></div><div>Number of ${selectedCrime}: ${f(d[selectedCrimeIndex])}</div>`
         + "<div>Population: " + d.population + "</div>")
         .style("left", (event.x) + 20 + "px")
         .style("top", (event.y + window.scrollY) + "px");
@@ -111,7 +121,15 @@
     };
 
     // Set bubble scale
-    const valueScale = d3.extent(crimeData, d => +d[selectedCrime]);
+    var valueScale = [0, 50000];
+    var legendValues = [2500, 5000, 10000];
+    if (selectedCrime === "Rapes") {
+      valueScale = [0, 5000]
+      legendValues = [400, 800, 1600];
+    } else if (selectedCrime === "Homicides") {
+      valueScale = [0, 2000]
+      legendValues = [100, 200, 400];
+    }
     const size = d3.scaleSqrt()
       .domain(valueScale)
       .range([1, 50]);
@@ -122,7 +140,7 @@
       .join("circle")
       .attr("cx", d => projection([+d.lng, +d.lat])[0])
       .attr("cy", d => projection([+d.lng, +d.lat])[1])
-      .attr("r", d => size(+d[selectedCrime]))
+      .attr("r", d => size(+d[selectedCrimeIndex]))
       .style("fill", d => selected === d.city ? "orange" : "#589BE5") // Set color based on selection
       .attr("stroke", "#0072BC")
       .attr("stroke-width", 0.5)
@@ -130,36 +148,164 @@
       .on("mouseover", mouseover)
       .on("mousemove", mousemove)
       .on("mouseleave", mouseleave)
-      .on("click", handleClick) 
+      .on("click", handleClick);
+
+    // Add Legend
+    const legendSize = legendValues.map(d => size(d));
+
+    const legend = svg.append("g")
+      .attr("class", "legend")
+      .attr("transform", "translate(50,50)");
+
+    legend.selectAll("circle")
+      .data(legendValues)
+      .enter()
+      .append("circle")
+      .attr("cx", 0)
+      .attr("cy", (d, i) => i * 50)
+      .attr("r", (d, i) => legendSize[i])
+      .style("fill", "#589BE5")
+      .style("opacity", 0.6);
+
+    legend.selectAll("text")
+      .data(legendValues)
+      .enter()
+      .append("text")
+      .attr("x", 40)
+      .attr("y", (d, i) => i * 50)
+      .attr("dy", "0.35em")
+      .text(d => `${Math.round(d)}  ${selectedCrime}`)
+      .style("font-size", "12px")
+      .style("fill", "#333");
+  }
+
+  function createHeatmap() {
+    const heatmapWidth = 600;
+    const heatmapHeight = 400;
+    const margin = { top: 30, right: 20, bottom: 40, left: 40 };
+    const cellSize = 50;
+    d3.select('#heatmap-container').selectAll('svg').remove();
+    const svg = d3.select("#heatmap-container")
+      .append("svg")
+      .attr("width", heatmapWidth + margin.left + margin.right)
+      .attr("height", heatmapHeight + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const cities = Object.keys(curCorr);
+    const numCols = Math.ceil(Math.sqrt(cities.length));
+    const numRows = Math.ceil(cities.length / numCols);
+
+    const colorScale = d3.scaleSequential(d3.interpolateRdYlGn)
+      .domain([-1, 1]);
+
+    const xScale = d3.scaleBand()
+      .range([0, heatmapWidth])
+      .domain(d3.range(numCols));
+
+    const yScale = d3.scaleBand()
+      .range([0, heatmapHeight])
+      .domain(d3.range(numRows));
+
+    svg.selectAll("rect")
+      .data(cities)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => xScale(i % numCols))
+      .attr("y", (d, i) => yScale(Math.floor(i / numCols)))
+      .attr("width", cellSize + 30)
+      .attr("height", cellSize)
+      .style("fill", d => colorScale(curCorr[d]));
+
+    svg.selectAll("text")
+      .data(cities)
+      .enter()
+      .append("text")
+      .attr("x", (d, i) => xScale(i % numCols) + cellSize / 2)
+      .attr("y", (d, i) => yScale(Math.floor(i / numCols)) + cellSize / 2)
+      .attr("dy", "2em")
+      .attr("text-anchor", "middle")
+      .style("font-size", "8px")
+      .text(d => d);
+
+    svg.append("text")
+      .attr("x", heatmapWidth / 2)
+      .attr("y", -10)
+      .attr("text-anchor", "middle")
+      .style("font-size", "16px")
+      .text(`Correlation between Unemployment and ${selectedCrime}`);
+
+    const legend = svg.append("g")
+      .attr("class", "legend")
+      .attr("transform", `translate(0,${heatmapHeight + 10})`);
+
+    const legendScale = d3.scaleLinear()
+      .range([0, heatmapWidth])
+      .domain([-1, 1]);
+
+    const legendAxis = d3.axisBottom(legendScale)
+      .ticks(5);
+
+    legend.selectAll("rect")
+      .data(d3.range(-1, 1.01, 0.01))
+      .enter()
+      .append("rect")
+      .attr("x", d => legendScale(d))
+      .attr("y", 0)
+      .attr("width", heatmapWidth / 200)
+      .attr("height", 10)
+      .style("fill", d => colorScale(d));
+
+    legend.append("g")
+      .attr("transform", `translate(0,10)`)
+      .call(legendAxis);
+  }
+
+  function handleYearChange(event) {
+    selectedYear = event.target.value;
+    crimeData = crime.filter(item => item.report_year === selectedYear);
+    updateMap();
+  }
+
+  function handleCrimeChange(event) {
+    selectedCrime = event.target.value;
+    selectedCrimeIndex = violent_crimes_dict[selectedCrime];
+    curCorr = corrData[selectedCrimeIndex]
+    updateMap();
+    createHeatmap()
   }
 
   function createLinePlot() {
-    // Clear previous line plot
+    if (!selected) return;
+
+    const crime_city = crime.filter(item => item.city === selected);
+
+    // Remove previous line plot if it exists
     d3.select('#line-plot-container').selectAll('svg').remove();
 
-    var crime_city = crime.filter(item => item.city === selected)
-    var svgs = d3.select('#line-plot-container').append('svg')
+    const svgs = d3.select('#line-plot-container')
+      .append('svg')
       .attr('width', linePlotWidth)
       .attr('height', linePlotHeight)
-      .style('border', '1px solid black'); // Add border to differentiate the line plot
+      .style('border', '1px solid black');
 
     // Set the ranges
-    var xScale = d3.scaleTime().range([40, linePlotWidth - 20]);
-    var yScaleCrime = d3.scaleLinear().range([linePlotHeight - 30, 20]);
-    var yScaleUnemployment = d3.scaleLinear().range([linePlotHeight - 30, 20]);
+    const xScale = d3.scaleTime().range([40, linePlotWidth - 20]);
+    const yScaleCrime = d3.scaleLinear().range([linePlotHeight - 30, 20]);
+    const yScaleUnemployment = d3.scaleLinear().range([linePlotHeight - 30, 20]);
 
     // Define the line functions
-    var crimeLine = d3.line()
+    const crimeLine = d3.line()
       .x(d => xScale(new Date(d.report_year)))
-      .y(d => yScaleCrime(+d[selectedCrime]));
+      .y(d => yScaleCrime(+d[selectedCrimeIndex]));
 
-    var unemploymentLine = d3.line()
+    const unemploymentLine = d3.line()
       .x(d => xScale(new Date(d.report_year)))
       .y(d => yScaleUnemployment(+d.unemploymentRate));
 
     // Scale the range of the data
     xScale.domain(d3.extent(crime_city, d => new Date(d.report_year)));
-    yScaleCrime.domain([0, d3.max(crime_city, d => +d[selectedCrime])]);
+    yScaleCrime.domain([0, d3.max(crime_city, d => +d[selectedCrimeIndex])]);
     yScaleUnemployment.domain([0, d3.max(crime_city, d => +d.unemploymentRate)]);
 
     // Add the crime path
@@ -205,12 +351,12 @@
       .text("Crime and Unemployment Rate Over Time");
 
     // Add legend
-    var legend = svgs.append("g")
+    const legend = svgs.append("g")
       .attr("transform", `translate(${linePlotWidth / 2 - 100}, ${linePlotHeight - 50})`);
 
     legend.append("circle")
       .attr("cx", 0)
-      .attr("cy", 0 )
+      .attr("cy", 0)
       .attr("r", 6)
       .style("fill", "blue");
 
@@ -234,25 +380,11 @@
       .style("font-size", "12px")
       .attr("alignment-baseline", "middle");
   }
-
-  function handleYearChange(event) {
-    selectedYear = event.target.value;
-    d3.csv('crime.csv').then(data => {
-      crimeData = data.filter(item => item.report_year === selectedYear);
-      updateMap();
-    });
-  }
-
-  function handleCrimeChange(event) {
-    selectedCrime = event.target.value;
-    updateMap();
-    createLinePlot();
-  }
 </script>
 
 <div id="container">
   <div id="controls">
-    <select bind:value={selectedYear} on:change={handleYearChange      }>
+    <select bind:value={selectedYear} on:change={handleYearChange}>
       {#each years as year}
         <option value={year}>{year}</option>
       {/each}
@@ -265,13 +397,12 @@
     </select>
   </div>
   <div id="map-container"></div>
-  <div id="line-plot-container"></div> <!-- Added container for the line plot -->
+  <div id="line-plot-container"></div>
+  <div id="heatmap-container"></div>
 </div>
 
 <style>
-  #container {
-    border: 2px solid black;
-    padding: 10px;
+  #container {  
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -279,8 +410,7 @@
   #controls {
     margin-bottom: 10px;
   }
-  #map-container, #line-plot-container {
+  #map-container, #line-plot-container, #heatmap-container {
     margin-top: 20px;
   }
 </style>
-
